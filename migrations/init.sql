7b0381a6c7f71ea3df6d60fb7a6267729a18beca
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2025-04-12 11:16:59 +0300

    chore: format changes

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,26 +1,26 @@
 CREATE TABLE IF NOT EXISTS pvz (
-                                   id UUID PRIMARY KEY,
-                                   registration_date TIMESTAMP NOT NULL DEFAULT NOW(),
+    id UUID PRIMARY KEY,
+    registration_date TIMESTAMP NOT NULL DEFAULT NOW(),
     city VARCHAR(50) NOT NULL CHECK (city IN ('Москва', 'Санкт-Петербург', 'Казань'))
     );
 
 CREATE TABLE IF NOT EXISTS reception (
-                                         id UUID PRIMARY KEY,
-                                         date_time TIMESTAMP NOT NULL DEFAULT NOW(),
+    id UUID PRIMARY KEY,
+    date_time TIMESTAMP NOT NULL DEFAULT NOW(),
     pvz_id UUID NOT NULL REFERENCES pvz(id),
     status VARCHAR(20) NOT NULL CHECK (status IN ('in_progress', 'close'))
     );
 
 CREATE TABLE IF NOT EXISTS product (
-                                       id UUID PRIMARY KEY,
-                                       date_time TIMESTAMP NOT NULL DEFAULT NOW(),
+    id UUID PRIMARY KEY,
+    date_time TIMESTAMP NOT NULL DEFAULT NOW(),
     type VARCHAR(20) NOT NULL CHECK (type IN ('электроника', 'одежда', 'обувь')),
     reception_id UUID NOT NULL REFERENCES reception(id)
     );
 
 CREATE TABLE IF NOT EXISTS users (
-                                     id UUID PRIMARY KEY,
-                                     email VARCHAR(255) UNIQUE NOT NULL,
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (role IN ('employee', 'moderator'))
     );
