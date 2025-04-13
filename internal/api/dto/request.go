dd51463cbc816d14d3617f3fee3d24b24057ce71
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2025-04-13 20:17:45 +0300

    feat: add handlers

--- internal/api/dto/request.go
+++ internal/api/dto/request.go
@@ -5,36 +5,36 @@ import (
 )
 
 type DummyLoginRequestDTO struct {
-	Role string `json:"role" validate:"required,oneof=employee moderator"`
+	Role string `json:"role" binding:"required,oneof=employee moderator"`
 }
 
 type RegisterRequestDTO struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6"`
-	Role     string `json:"role" validate:"required,oneof=employee moderator"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+	Role     string `json:"role" binding:"required,oneof=employee moderator"`
 }
 
 type LoginRequestDTO struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
 
 type PVZRequestDTO struct {
-	City string `json:"city" validate:"required,oneof=Москва Санкт-Петербург Казань"`
+	City string `json:"city" binding:"required,oneof=Москва Санкт-Петербург Казань"`
 }
 
 type PVZListFilterDTO struct {
-	StartDate *time.Time `json:"startDate" validate:"omitempty"`
-	EndDate   *time.Time `json:"endDate" validate:"omitempty,gtfield=StartDate"`
-	Page      int        `json:"page" validate:"omitempty,min=1"`
-	Limit     int        `json:"limit" validate:"omitempty,min=1,max=30"`
+	StartDate *time.Time `json:"startDate" form:"startDate" binding:"omitempty"`
+	EndDate   *time.Time `json:"endDate" form:"endDate" binding:"omitempty,gtfield=StartDate"`
+	Page      int        `json:"page" form:"page" binding:"omitempty,min=1"`
+	Limit     int        `json:"limit" form:"limit" binding:"omitempty,min=1,max=30"`
 }
 
 type ReceptionCreateRequestDTO struct {
-	PVZID string `json:"pvzId" validate:"required,uuid4"`
+	PVZID string `json:"pvzId" binding:"required,uuid4"`
 }
 
 type ProductCreateRequestDTO struct {
-	Type  string `json:"type" validate:"required,oneof=электроника одежда обувь"`
-	PVZID string `json:"pvzId" validate:"required,uuid4"`
+	Type  string `json:"type" binding:"required,oneof=электроника одежда обувь"`
+	PVZID string `json:"pvzId" binding:"required,uuid4"`
 }
